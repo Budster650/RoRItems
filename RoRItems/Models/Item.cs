66862c89dc9c21f28305a13dfc2f02b4b31e3402
Mathias Richardson <reversesectionsign@gmail.com>
2019-10-21 18:43:42 -0400

    Add project files.

---
+++ RoRItems/Models/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace RoRItems.Models
+{
+    public class Item
+    {
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string Name { get; set; }
+        public string Rarity { get; set; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize<Item>(this);
+        }
+    }
+}
