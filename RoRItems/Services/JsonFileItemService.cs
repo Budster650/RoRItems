d36775ada1e8f52fdd5abb1f588a92a926332bd7
Mathias Richardson <reversesectionsign@gmail.com>
2019-10-24 11:31:33 -0400

    added item service and default view for items

---
+++ RoRItems/Services/JsonFileItemService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using RoRItems.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace RoRItems.Services
+{
+    public class JsonFileItemService
+    {
+        public JsonFileItemService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "items.json"); }
+        }
+
+        public IEnumerable<Item> GetItems()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Item[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+    }
+}
