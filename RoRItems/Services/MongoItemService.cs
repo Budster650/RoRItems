86ff61bc46aadc745eda98905fb95f4a504e0f04
Mathias Richardson <reversesectionsign@gmail.com>
2019-11-06 13:45:53 -0500

    added mongodb driver and empty service

---
+++ RoRItems/Services/MongoItemService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using RoRItems.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace RoRItems.Services
+{
+    public class MongoItemService
+    {
+
+        public IEnumerable<Item> GetItems()
+        {
+            /*
+             * From JsonFileItemService, for reference:
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Item[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+            */
+            return JsonSerializer.Deserialize<Item[]>("");
+        }
+    }
+}
